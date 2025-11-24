b262866a9e4d8ba4e3c9edd9e4a963f03233e798
quietxhub99 <hubquiet199>
2025-11-25 04:15:56 +0700

    update warsa.lua

--- fix/warsa.lua
+++ fix/warsa.lua
@@ -3819,7 +3819,7 @@ local REObtainedNewFishNotification = ReplicatedStorage.Packages._Index["sleitni
 local webhookPath = nil
 local FishWebhookEnabled = true
 local LastCatchData = {}
-local SelectedCategories = { "Secret", "Mythic" }
+local SelectedCategories = { "Secret" }
 
 -------------------------------------------
 ----- =======[ HELPER FUNCTIONS ]
